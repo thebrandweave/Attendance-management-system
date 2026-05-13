50021815ad5aa4a66208e2b49ee25ef242225278
mufeez123-lab <mufizmalar@gmail.com>
2026-05-13 09:57:10 +0530

    updated editing functionality in dashboard admin

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -509,6 +509,7 @@ if ($totalHours !== "-") {
           <option value="Late">Late</option>
           <option value="Half Day">Half Day</option>
           <option value="Pending">Pending</option>
+          <option value="Absent">Absent</option>
           <option value="Overtime">Overtime</option>
 
       </select>
