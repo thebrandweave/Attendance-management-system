21c26487eee6dde3b465d30dfc160a5e562c8978
mufeez123-lab <mufizmalar@gmail.com>
2026-05-13 10:08:40 +0530

    13/05/26

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -296,7 +296,7 @@ if ($isNewEmployee) {
       if ($m['status'] == "Present" || $m['status']=="Late"){ $present++;
       }
       elseif ($m['status'] == "Half Day") $half++;
-      elseif ($m['status'] == "Pending" || $m['status'=="Absent"]){ $absent++;
+      elseif ($m['status'] == "Pending" || $m['status']=="Absent"){ $absent++;
       }
     }
   }
