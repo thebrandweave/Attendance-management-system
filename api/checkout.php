99f96da9b42bbbb883325638a3df28f49efdc215
thebrandweave <wearebrandweave@gmail.com>
2026-06-05 14:35:52 +0530

    updated leave page

--- api/checkout.php
+++ api/checkout.php
@@ -119,7 +119,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['token'])) {
             $checkoutTimeOnly = date("H:i", $checkOut);
 
             // Unified Status Resolution Logic
-            if ($totalHours < 4) {
+            if ($totalHours < 4.5) {
                 $status = "Half Day";
             } elseif ($checkoutTimeOnly >= "17:35") {
                 $status = "Overtime";
